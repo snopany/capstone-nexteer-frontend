19b7377cdbc8fdda4c9d7ca181a13494448b3b8f
snopany <snopany@andrew.cmu.edu>
2025-07-17 22:06:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ def upload_pdf():
 def open_browser():
     webbrowser.open_new("http://127.0.0.1:5000/")
 
-if __name__ == '__main__':
-    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
-        threading.Timer(1, open_browser).start()
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+#         threading.Timer(1, open_browser).start()
+#     app.run(debug=True)
 
